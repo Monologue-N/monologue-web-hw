136954848d3698222f90fa4988a8c5543910d7d0
Monologue.№ <cancan@usc.edu>
2021-04-07 18:21:52 -0700

    fix continue watching carousel

--- ng-hw8/ng-usc-films/src/app/components/mixed-carousel/mixed-carousel.component.ts
+++ ng-hw8/ng-usc-films/src/app/components/mixed-carousel/mixed-carousel.component.ts
@@ -7,6 +7,75 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
   styleUrls: ['./mixed-carousel.component.css']
 })
 export class MixedCarouselComponent implements OnInit {
+  // @Input() data: any;
+  // @Input() continue: any;
+  // public mobile: any;
+  // public dataArray: any = [];
+  // myStorage = window.localStorage;
+  // isMobile: any;
+  // isDesktop: any;
+  //
+  // constructor(public breakpointObserver: BreakpointObserver) { }
+  //
+  // ngOnInit(): void {
+  //   // if (window.screen.width === 360) { // 768px portrait
+  //   //   this.mobile = true;
+  //   // }
+  //   // console.log('continue is ' + this.continue);
+  //   // console.log('array is  ' + this.data);
+  //   this.isMobile = this.breakpointObserver.isMatched('(max-width: 500px)');
+  //   this.isDesktop = this.breakpointObserver.isMatched('(min-width: 500px)');
+  //
+  //   if (this.continue !== 'true') {
+  //     this.data = this.data.results;
+  //     // console.log('check if continue');
+  //   }
+  //   if (this.isDesktop) {
+  //     this.transformData();
+  //   } else if (this.isMobile){
+  //     this.transformData2();
+  //   }
+  // }
+  //
+  // transformData() {
+  //   // console.log(this.data);
+  //   let j = -1;
+  //   if (this.data) {
+  //     for (let i = 0; i < this.data.length && i < 24; i++) {
+  //       // console.log(this.data[i].poster_path);
+  //       if (i % 6 === 0) {
+  //         j++;
+  //         this.dataArray[j] = [];
+  //         this.dataArray[j].push(this.data[i]);
+  //       }
+  //       else {
+  //         this.dataArray[j].push(this.data[i]);
+  //       }
+  //     }
+  //   }
+  //   // console.log(this.dataArray);
+  // }
+  //
+  // transformData2() {
+  //   let j = -1;
+  //   if (this.data) {
+  //     for (let i = 0; i < this.data.length && i < 24; i++) {
+  //       // console.log(this.data[i].poster_path);
+  //       if (i % 1 === 0) {
+  //         j++;
+  //         this.dataArray[j] = [];
+  //         this.dataArray[j].push(this.data[i]);
+  //       }
+  //       else {
+  //         this.dataArray[j].push(this.data[i]);
+  //       }
+  //     }
+  //   }
+  // }
+  //
+  // reloadPage(id: any) {
+  //   window.location.href = `/watch/movie/${id}`;
+  // }
   @Input() data: any;
   @Input() continue: any;
   public mobile: any;
@@ -21,11 +90,10 @@ export class MixedCarouselComponent implements OnInit {
     // if (window.screen.width === 360) { // 768px portrait
     //   this.mobile = true;
     // }
-    // console.log('continue is ' + this.continue);
-    // console.log('array is  ' + this.data);
     this.isMobile = this.breakpointObserver.isMatched('(max-width: 500px)');
     this.isDesktop = this.breakpointObserver.isMatched('(min-width: 500px)');
-
+    // console.log('continue is ' + this.continue);
+    // console.log('array is  ' + this.data);
     if (this.continue !== 'true') {
       this.data = this.data.results;
       // console.log('check if continue');
@@ -57,6 +125,7 @@ export class MixedCarouselComponent implements OnInit {
   }
 
   transformData2() {
+    // console.log(this.data);
     let j = -1;
     if (this.data) {
       for (let i = 0; i < this.data.length && i < 24; i++) {
@@ -71,6 +140,7 @@ export class MixedCarouselComponent implements OnInit {
         }
       }
     }
+    // console.log(this.dataArray);
   }
 
   reloadPage(id: any) {
